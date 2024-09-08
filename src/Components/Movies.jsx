ace8b6285fac62fa4637246bd7a3d59e680e5efa
Akshay Kalangi <akshaykalangi9@gmail.com>
2024-09-08 16:00:42 +0530

    updated

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -8,7 +8,11 @@ const Movies = ({handleWatchList,removeWatchList,watchList}) => {
   
 
   useEffect(() => {
-    axios.get(`https://www.omdbapi.com/?s=Batman&page=${pageNo}&apikey=171b626c`).then((res) => {
+    axios.get(`https://www.omdbapi.com/?s=Batman&page=${pageNo}&apikey=171b626c`,{
+      params:{
+        limit:20
+      }
+    }).then((res) => {
       setMovies(res.data.Search); 
       console.log(res.data);
     }).catch((error) => {
@@ -48,13 +52,14 @@ const Movies = ({handleWatchList,removeWatchList,watchList}) => {
         flexDirection: "row",
         justifyContent: "space-around",
         flexWrap: "wrap",
-        flex:"1"
+        flex:"2"
       }}>
         {movies.map((movie, index) => (
           <MovieCard key={index} movieObj={movie}  name={movie.Title} moviePath={movie.Poster} handleWatchList={handleWatchList} removeWatchList={removeWatchList} watchList={watchList}/>
         ))}
       </div>
-      <Pagination page={pageNo} decrement={decrementPage} increment={incrementPage} />    </div>
+      <Pagination page={pageNo} decrement={decrementPage} increment={incrementPage} />   
+       </div>
   );
 };
 
